a421d318baf27fa5e731333f3f723e67b3c0aee8
Matt Zamora <mzamora2899@gmail.com>
2023-09-02 12:38:07 -0600

    add eslint ignore for any

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { $user } from '$store/user';
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 const baseURL = import.meta.env?.VITE_API_URL;
 
 const axiosInstance = axios.create({

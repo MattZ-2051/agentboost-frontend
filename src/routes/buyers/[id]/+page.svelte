95effefffd9c2a6808572a78093c4358bcc5e096
Matt Zamora <mzamora2899@gmail.com>
2023-09-11 19:35:45 -0600

    update buyers ui

--- src/routes/buyers/[id]/+page.svelte
+++ src/routes/buyers/[id]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import Button from '$lib/components/Button/Button.svelte';
   import BuyersTile from '$lib/components/BuyersTile/BuyersTile.svelte';
+  import Slider from '$lib/components/CardSlider/Slider.svelte';
+  import { sliderItems } from '$mockData/listings';
   let buyersTiles = [{ id: 1 }, { id: 2 }, { id: 3 }];
 </script>
 
@@ -9,4 +12,16 @@
       <BuyersTile />
     </div>
   {/each}
+  <div class="grid grid-cols-2 gap-x-12 mt-12">
+    <Button
+      label="Navigate to Schedule"
+      variant="variant-filled-secondary"
+      classes="w-full text-sm mt-4 p-2"
+    />
+    <Button
+      label="Export to Buyer PDF"
+      variant="variant-filled-secondary"
+      classes="w-full text-sm mt-4 p-2"
+    />
+  </div>
 </div>

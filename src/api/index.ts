25620c0120b8ba606a32b93f1bd949e274b4f9e4
Matt Zamora <mzamora2899@gmail.com>
2023-11-01 20:56:30 -0600

    wip

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { $user } from '$store/user';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// @ts-ignore
+//@ts-ignore
 const baseURL = import.meta.env?.VITE_API_URL;
 
 const axiosInstance = axios.create({

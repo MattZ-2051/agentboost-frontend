9036cc082a315745c92f616e60fe186a8dba7cfb
Matt Zamora <mzamora2899@gmail.com>
2023-11-05 16:43:44 -0700

    landing page wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -104,11 +104,9 @@
     '!w-[8rem] !text-opacity-40 !text-white hover:!text-primary-500 hover:!bg-white hover:!bg-opacity-10';
 </script>
 
-<div class="relative w-full h-full">
+<div class="w-full h-full relative">
   <div class="elipse absolute" />
-  <section
-    class="w-full flex flex-col h-full items-center justify-center relative"
-  >
+  <section class="w-full flex flex-col h-full items-center justify-center">
     <div class="flex flex-col items-center justify-center">
       <div class="w-[278px] h-[39px] relative">
         <div
@@ -174,10 +172,15 @@
       </p>
     </div>
     <div
-      class="w-[1188px] h-[643px] bg-white bg-opacity-0 rounded-[19px] shadow border border-white border-opacity-40"
+      class="p-1 bg-zinc-950 bg-opacity-0 rounded-[20px] border border-white border-opacity-5"
     >
-      <video controls width="100%" />
+      <div
+        class="w-[1188px] h-[643px] bg-white bg-opacity-0 rounded-[19px] shadow border border-white border-opacity-40"
+      >
+        <video controls width="100%" />
+      </div>
     </div>
+
     <div class="mt-12 flex justify-center w-full">
       <div
         class="flex items-center leading-9 text-[2rem] text-white font-normal"
@@ -196,7 +199,7 @@
     <p class="text-6xl text-center">
       Effortless <span class="textGradient"> expertise & exposure</span>
     </p>
-    <div class="text-center my-10 text-lg text-slate font-light">
+    <div class="text-center my-6 text-lg text-slate font-light">
       <p>Get your competitive advantage. Get Boosted.</p>
     </div>
     <div class="w-full flex flex-col items-center justify-center">
@@ -234,7 +237,7 @@
         <div
           class="w-[1188px] h-[490px] border-opacity-20 bg-black bg-opacity-30 rounded-[18px] shadow border border-white backdrop-blur-[14.35px] flex flex-col items-center p-12"
         >
-          <div class="flex items-center absolute opacity-100">
+          <div class="flex items-center opacity-100">
             <div class="mr-4">
               <LandingBezier />
             </div>
@@ -332,66 +335,66 @@
           </div>
         </div>
       </div>
-      <div class="mt-12 flex justify-between w-[1188px]">
-        <div class="w-[576px] h-[179px] relative z-10">
-          <div
-            class="w-[576px] h-[179px] left-0 top-0 absolute bg-gradient-to-br from-blue-500 to-teal-300 rounded-[18px]"
-          />
-          <div
-            class="w-[815px] h-[248px] left-[-59px] top-[-100px] absolute bg-black rounded-full blur-[100px]"
-          />
-          <div class="absolute m-2 flex items-center w-full">
-            <img
-              class="w-[133px] h-[155px] rounded-[17px]"
-              src="https://via.placeholder.com/133x155"
-            />
-            <div class="ml-4 whitespace-nowrap">
-              <p class="text-white text-[28px] leading-[35px]">Sam Hamilton</p>
-              <p class="text-slate-400 text-[17px] font-normal leading-[25px]">
-                Founder & CEO
-              </p>
-            </div>
-            <div class="flex justify-end mr-8 w-full gap-x-4">
-              <Instagram />
-              <Twitter />
+      <div class="mt-12 flex justify-center gap-x-8">
+        <div
+          class="p-1 bg-zinc-950 bg-opacity-0 rounded-[20px] border border-white border-opacity-5"
+        >
+          <div class="w-[576px] h-[179px] relative z-10 cta rounded-[18px]">
+            <div class="absolute m-2 flex items-center w-full">
+              <img
+                class="w-[133px] h-[155px] rounded-[17px]"
+                src="https://via.placeholder.com/133x155"
+              />
+              <div class="ml-4 whitespace-nowrap">
+                <p class="text-white text-[28px] leading-[35px]">
+                  Sam Hamilton
+                </p>
+                <p
+                  class="text-slate-400 text-[17px] font-normal leading-[25px]"
+                >
+                  Founder & CEO
+                </p>
+              </div>
+              <div class="flex justify-end mr-8 w-full gap-x-4">
+                <Instagram />
+                <Twitter />
+              </div>
             </div>
           </div>
         </div>
-        <div class="w-[576px] h-[179px] relative z-10">
-          <div
-            class="w-[576px] h-[179px] left-0 top-0 absolute bg-gradient-to-br from-blue-500 to-teal-300 rounded-[18px]"
-          />
-          <div
-            class="w-[815px] h-[248px] left-[-59px] top-[-100px] absolute bg-black rounded-full blur-[100px]"
-          />
-          <div class="absolute m-2 flex items-center w-full">
-            <img
-              class="w-[133px] h-[155px] object-cover rounded-[17px]"
-              src={matt}
-            />
-            <div class="ml-4 whitespace-nowrap">
-              <p class="text-white text-[28px] leading-[35px]">Matt Zamora</p>
-              <p class="text-slate-400 text-[17px] font-normal leading-[25px]">
-                Tech Lead
-              </p>
-            </div>
-            <div class="flex justify-end mr-8 w-full gap-x-4">
-              <Instagram />
-              <Twitter />
+        <div
+          class="p-1 w-full bg-zinc-950 bg-opacity-0 rounded-[20px] border border-white border-opacity-5"
+        >
+          <div class="w-[576px] h-[179px] relative z-10 cta rounded-[18px]">
+            <div class="absolute m-2 flex items-center w-full">
+              <img
+                class="w-[133px] h-[155px] object-cover rounded-[17px]"
+                src={matt}
+              />
+              <div class="ml-4 whitespace-nowrap">
+                <p class="text-white text-[28px] leading-[35px]">Matt Zamora</p>
+                <p
+                  class="text-slate-400 text-[17px] font-normal leading-[25px]"
+                >
+                  Tech Lead
+                </p>
+              </div>
+              <div class="flex justify-end mr-8 w-full gap-x-4">
+                <Instagram />
+                <Twitter />
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </section>
-  <section class="mt-20 flex justify-center mx-32">
+  <section class="mt-36 mb-12 flex justify-center mx-28">
     <div
       class="p-1 w-full bg-zinc-950 bg-opacity-0 rounded-[20px] border border-white border-opacity-10"
     >
       <div class="w-full h-[500px] relative">
-        <div
-          class="absolute top-0 w-full h-full rounded-bl-[40px] rounded-[20px] cta"
-        >
+        <div class="absolute top-0 w-full h-full rounded-[20px] cta">
           <div
             class="w-full h-[539px] relative flex justify-center items-center"
           >
@@ -468,13 +471,10 @@
     background-blend-mode: color-dodge;
   }
 
-  .cta-mask {
-    border-radius: 1.125rem;
-    background: linear-gradient(113deg, #010101 3.51%, #010101 111.71%);
-  }
   .cta {
-    /* background: linear-gradient(113deg, #010101 3.51%, #010101 111.71%); */
-    background: linear-gradient(140deg, black 50%, #3d7fff 140%, #49f9ea 110%);
+    /* background: linear-gradient(180deg, black 50%, #3d7fff 140%, #49f9ea 110%); */
+    background: linear-gradient(to bottom, black 45%, rgba(0, 0, 0, 0.3)),
+      linear-gradient(to right, #3d7fff 60%, #49f9ea 110%);
   }
 
   #text1,

264a42519e9ed9c5c8ce58c6a53d4d01f36b2ea7
Matt Zamora <mzamora2899@gmail.com>
2023-11-06 19:20:38 -0700

    fix errors

--- src/lib/components/Calender/Calender.svelte
+++ src/lib/components/Calender/Calender.svelte
@@ -1,268 +1,262 @@
 <script lang="ts">
-  import { dndzone, type DndEvent } from 'svelte-dnd-action';
-  import ArrowLeft from '$lib/icons/Arrow/ArrowLeft.svelte';
-  import ArrowRight from '$lib/icons/Arrow/ArrowRight.svelte';
-  import CardModal from '../Modals/CardModal/CardModal.svelte';
-  import Chip from '../Chip/Chip.svelte';
-  import {
-    modalStore,
-    type ModalSettings,
-    type ModalComponent,
-  } from '@skeletonlabs/skeleton';
-  import { onMount } from 'svelte';
+	import { dndzone, type DndEvent } from 'svelte-dnd-action';
+	import ArrowLeft from '$lib/icons/Arrow/ArrowLeft.svelte';
+	import ArrowRight from '$lib/icons/Arrow/ArrowRight.svelte';
+	import CardModal from '../Modals/CardModal/CardModal.svelte';
+	import Chip from '../Chip/Chip.svelte';
+	import { getModalStore, type ModalSettings, type ModalComponent } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
 
-  const modalComponent: ModalComponent = {
-    // Pass a reference to your custom component
-    ref: CardModal,
-    // Add the component properties as key/value pairs
-    props: {},
-    // Provide a template literal for the default component slot
-    slot: '<p>Skeleton</p>',
-  };
-  // Provide the modal settings
-  const modal: ModalSettings = {
-    type: 'component',
-    component: modalComponent,
-  };
+	const modalStore = getModalStore();
+	const modalComponent: ModalComponent = {
+		// Pass a reference to your custom component
+		ref: CardModal,
+		// Add the component properties as key/value pairs
+		props: {},
+		// Provide a template literal for the default component slot
+		slot: '<p>Skeleton</p>'
+	};
+	// Provide the modal settings
+	const modal: ModalSettings = {
+		type: 'component',
+		component: modalComponent
+	};
 
-  const date = new Date();
+	const date = new Date();
 
-  const today = {
-    dayNumber: date.getDate(),
-    month: date.getMonth(),
-    year: date.getFullYear(),
-  };
+	const today = {
+		dayNumber: date.getDate(),
+		month: date.getMonth(),
+		year: date.getFullYear()
+	};
 
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+	const monthNames = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
 
-  let monthIndex = date.getMonth();
-  // let monthIndex = 9;
-  // const currentMonth = date.toLocaleString('en-US', { month: 'long' })
-  $: month = monthNames[monthIndex];
+	let monthIndex = date.getMonth();
+	// let monthIndex = 9;
+	// const currentMonth = date.toLocaleString('en-US', { month: 'long' })
+	$: month = monthNames[monthIndex];
 
-  let year = date.getFullYear();
+	let year = date.getFullYear();
 
-  $: firstDayIndex = new Date(year, monthIndex, 1).getDay();
-  // const currentDay = date.getDate();
-  $: numberOfDays = new Date(year, monthIndex + 1, 0).getDate();
+	$: firstDayIndex = new Date(year, monthIndex, 1).getDay();
+	// const currentDay = date.getDate();
+	$: numberOfDays = new Date(year, monthIndex + 1, 0).getDate();
 
-  $: calendarCellsQty = numberOfDays + firstDayIndex;
+	$: calendarCellsQty = numberOfDays + firstDayIndex;
 
-  $: calender = monthIndex !== undefined ? handleCalender() : [];
+	$: calender = monthIndex !== undefined ? handleCalender() : [];
 
-  type CalenderItem = {
-    id: number;
-    items: ListingItem[];
-  };
+	type CalenderItem = {
+		id: number;
+		items: ListingItem[];
+	};
 
-  const handleCalender = (): CalenderItem[] => {
-    const days = new Array(calendarCellsQty);
-    const result: CalenderItem[] = [];
-    for (let i = 0; i < days.length; i++) {
-      const day = i - firstDayIndex + 1;
-      const item = items.filter((item) => parseInt(item.day, 10) === day);
+	const handleCalender = (): CalenderItem[] => {
+		const days = new Array(calendarCellsQty);
+		const result: CalenderItem[] = [];
+		for (let i = 0; i < days.length; i++) {
+			const day = i - firstDayIndex + 1;
+			const item = items.filter((item) => parseInt(item.day, 10) === day);
 
-      result.push({
-        id: i + 1,
-        items: [...item],
-      });
-    }
+			result.push({
+				id: i + 1,
+				items: [...item]
+			});
+		}
 
-    return result;
-  };
+		return result;
+	};
 
-  onMount(() => {
-    calender = handleCalender();
-  });
+	onMount(() => {
+		calender = handleCalender();
+	});
 
-  const goToNextMonth = () => {
-    if (monthIndex >= 11) {
-      year += 1;
-      return (monthIndex = 0);
-    }
-    return (monthIndex += 1);
-  };
+	const goToNextMonth = () => {
+		if (monthIndex >= 11) {
+			year += 1;
+			return (monthIndex = 0);
+		}
+		return (monthIndex += 1);
+	};
 
-  const goToPrevMonth = () => {
-    if (monthIndex <= 0) {
-      year -= 1;
-      return (monthIndex = 11);
-    }
-    return (monthIndex -= 1);
-  };
+	const goToPrevMonth = () => {
+		if (monthIndex <= 0) {
+			year -= 1;
+			return (monthIndex = 11);
+		}
+		return (monthIndex -= 1);
+	};
 
-  type ListingItem = {
-    id: number;
-    label: string;
-    day: string;
-    month: string;
-  };
+	type ListingItem = {
+		id: number;
+		label: string;
+		day: string;
+		month: string;
+	};
 
-  let items: ListingItem[] = [
-    {
-      id: 1,
-      label: 'listing 1',
-      day: '10',
-      month: 'September',
-    },
-    {
-      id: 2,
-      label: 'listing 2',
-      day: '13',
-      month: 'August',
-    },
-    {
-      id: 3,
-      label: 'listing 3',
-      day: '10',
-      month: 'September',
-    },
-  ];
+	let items: ListingItem[] = [
+		{
+			id: 1,
+			label: 'listing 1',
+			day: '10',
+			month: 'September'
+		},
+		{
+			id: 2,
+			label: 'listing 2',
+			day: '13',
+			month: 'August'
+		},
+		{
+			id: 3,
+			label: 'listing 3',
+			day: '10',
+			month: 'September'
+		}
+	];
 
-  const handleModal = () => {
-    modalStore.trigger(modal);
-  };
+	const handleModal = () => {
+		modalStore.trigger(modal);
+	};
 
-  const handleItemConsider = (
-    id: number,
-    e: CustomEvent<DndEvent<ListingItem>>,
-  ) => {
-    const colIdx = calender.findIndex((c) => c.id === id);
-    calender[colIdx].items = e.detail.items;
-    calender = [...calender];
-  };
+	const handleItemConsider = (id: number, e: CustomEvent<DndEvent<ListingItem>>) => {
+		const colIdx = calender.findIndex((c) => c.id === id);
+		calender[colIdx].items = e.detail.items;
+		calender = [...calender];
+	};
 
-  const handleItemFinalize = (
-    id: number,
-    itemInfo: { day: string; month: string },
-    e: CustomEvent<DndEvent<ListingItem>>,
-  ) => {
-    const colIdx = calender.findIndex((c) => c.id === id);
-    for (const info of e.detail.items) {
-      const { day, month } = itemInfo;
-      info.day = day;
-      info.month = month;
-    }
-    calender[colIdx].items = e.detail.items;
-    calender = [...calender];
-  };
+	const handleItemFinalize = (
+		id: number,
+		itemInfo: { day: string; month: string },
+		e: CustomEvent<DndEvent<ListingItem>>
+	) => {
+		const colIdx = calender.findIndex((c) => c.id === id);
+		for (const info of e.detail.items) {
+			const { day, month } = itemInfo;
+			info.day = day;
+			info.month = month;
+		}
+		calender[colIdx].items = e.detail.items;
+		calender = [...calender];
+	};
 </script>
 
 <div class="w-full flex justify-center">
-  <div class="bg-secondary-600 h-full w-[1000px] p-12">
-    <div class="flex justify-between">
-      <div
-        class="hover:cursor-pointer hover:scale-110"
-        on:click={goToPrevMonth}
-        on:keydown={goToPrevMonth}
-      >
-        <ArrowLeft />
-      </div>
-      <h1 class="text-3xl">{month}</h1>
-      <div
-        class="hover:cursor-pointer hover:scale-110"
-        on:click={goToNextMonth}
-        on:keydown={goToNextMonth}
-      >
-        <ArrowRight />
-      </div>
-    </div>
-    <hr class="my-8" />
-    <ul class="weekdays grid grid-cols-7">
-      <li>Su</li>
-      <li>Mo</li>
-      <li>Tu</li>
-      <li>We</li>
-      <li>Th</li>
-      <li>Fr</li>
-      <li>Sa</li>
-    </ul>
-    <ul class="days grid grid-cols-7">
-      {#each calender as item, i (item.id)}
-        {#if i < firstDayIndex || i >= numberOfDays + firstDayIndex}
-          <div class="cell">&nbsp;</div>
-        {:else}
-          <div
-            use:dndzone={{
-              items: item.items,
-              dropTargetStyle: {},
-            }}
-            id={`${i - firstDayIndex + 1}`}
-            class="cell flex flex-col h-full"
-            on:consider={(e) => handleItemConsider(item.id, e)}
-            on:finalize={(e) =>
-              handleItemFinalize(
-                item.id,
-                { day: (i - firstDayIndex + 1).toString(), month: month },
-                e,
-              )}
-          >
-            {i - firstDayIndex + 1}
-            {#each item.items as info, x (info.id)}
-              {#if info.month === month}
-                <div class={`${x > 0 ? 'mb-2' : ''}`}>
-                  <Chip title={info.label} variant="variant-filled" />
-                </div>
-              {/if}
-            {/each}
-          </div>{/if}
-      {/each}
-    </ul>
-  </div>
+	<div class="bg-secondary-600 h-full w-[1000px] p-12">
+		<div class="flex justify-between">
+			<div
+				class="hover:cursor-pointer hover:scale-110"
+				on:click={goToPrevMonth}
+				on:keydown={goToPrevMonth}
+			>
+				<ArrowLeft />
+			</div>
+			<h1 class="text-3xl">{month}</h1>
+			<div
+				class="hover:cursor-pointer hover:scale-110"
+				on:click={goToNextMonth}
+				on:keydown={goToNextMonth}
+			>
+				<ArrowRight />
+			</div>
+		</div>
+		<hr class="my-8" />
+		<ul class="weekdays grid grid-cols-7">
+			<li>Su</li>
+			<li>Mo</li>
+			<li>Tu</li>
+			<li>We</li>
+			<li>Th</li>
+			<li>Fr</li>
+			<li>Sa</li>
+		</ul>
+		<ul class="days grid grid-cols-7">
+			{#each calender as item, i (item.id)}
+				{#if i < firstDayIndex || i >= numberOfDays + firstDayIndex}
+					<div class="cell">&nbsp;</div>
+				{:else}
+					<div
+						use:dndzone={{
+							items: item.items,
+							dropTargetStyle: {}
+						}}
+						id={`${i - firstDayIndex + 1}`}
+						class="cell flex flex-col h-full"
+						on:consider={(e) => handleItemConsider(item.id, e)}
+						on:finalize={(e) =>
+							handleItemFinalize(
+								item.id,
+								{ day: (i - firstDayIndex + 1).toString(), month: month },
+								e
+							)}
+					>
+						{i - firstDayIndex + 1}
+						{#each item.items as info, x (info.id)}
+							{#if info.month === month}
+								<div class={`${x > 0 ? 'mb-2' : ''}`}>
+									<Chip title={info.label} variant="variant-filled" />
+								</div>
+							{/if}
+						{/each}
+					</div>{/if}
+			{/each}
+		</ul>
+	</div>
 </div>
 
 <style>
-  ul {
-    list-style-type: none;
-  }
+	ul {
+		list-style-type: none;
+	}
 
-  /* Weekdays (Mon-Sun) */
-  .weekdays {
-    margin: 0;
-    padding: 10px 0;
-  }
+	/* Weekdays (Mon-Sun) */
+	.weekdays {
+		margin: 0;
+		padding: 10px 0;
+	}
 
-  .weekdays li {
-    display: inline-block;
-    border: 1px solid black;
-    text-align: center;
-  }
+	.weekdays li {
+		display: inline-block;
+		border: 1px solid black;
+		text-align: center;
+	}
 
-  /* Days (1-31) */
-  .days {
-    padding: 10px 0;
-    margin: 0;
-  }
+	/* Days (1-31) */
+	.days {
+		padding: 10px 0;
+		margin: 0;
+	}
 
-  .cell {
-    list-style-type: none;
-    border: 1px solid black;
-    padding: 9px;
-    margin: 0 2px;
-    height: 7rem;
-    text-align: center;
-    margin-bottom: 1px;
-    font-size: 1.2rem;
-    cursor: pointer;
-  }
+	.cell {
+		list-style-type: none;
+		border: 1px solid black;
+		padding: 9px;
+		margin: 0 2px;
+		height: 7rem;
+		text-align: center;
+		margin-bottom: 1px;
+		font-size: 1.2rem;
+		cursor: pointer;
+	}
 
-  /* Highlight the "current" day */
-  .active {
-    padding: 5px;
-    /* @apply bg-secondary-400; */
-    color: white !important;
-  }
+	/* Highlight the "current" day */
+	.active {
+		padding: 5px;
+		/* @apply bg-secondary-400; */
+		color: white !important;
+	}
 </style>
--- src/lib/components/Modals/CalenderAdd/CalenderAdd.svelte
+++ src/lib/components/Modals/CalenderAdd/CalenderAdd.svelte
@@ -1,54 +1,39 @@
 <script lang="ts">
-  import Input from '$lib/components/Input/Input.svelte';
-  import Button from '$lib/components/Button/Button.svelte';
-  import { modalStore } from '@skeletonlabs/skeleton';
-  import { addGmcToCalender } from '$api/gmc';
+	import Input from '$lib/components/Input/Input.svelte';
+	import Button from '$lib/components/Button/Button.svelte';
+	import { getModalStore } from '@skeletonlabs/skeleton';
+	import { addGmcToCalender } from '$api/gmc';
 
-  let price: string = '';
-  let startDate: string = '';
+	let price: string = '';
+	let startDate: string = '';
 
-  const handleCalenderAdd = async () => {
-    const listingId = window.location.pathname.split('/')[2];
-    await addGmcToCalender({ startDate, listingId });
-  };
+	const modalStore = getModalStore();
+	const handleCalenderAdd = async () => {
+		const listingId = window.location.pathname.split('/')[2];
+		await addGmcToCalender({ startDate, listingId });
+	};
 </script>
 
 <div class="card p-8 flex flex-col justify-between w-[500px]">
-  <div class="flex flex-col justify-center items-center">
-    <p class="text-2xl mt-4 font-semibold">
-      Add a new listing to your calender
-    </p>
-    <p class="px-4 mt-4">
-      Choose the date and price you want to start your content at, after that
-      each item will be posted every week, you can adjust your settings in the
-      marketing page
-    </p>
-  </div>
-  <div class="my-8">
-    <div class="mb-4">
-      <Input label="Price" placeholder="Price" type="text" bind:value={price} />
-    </div>
-    <div>
-      <Input
-        label="Date"
-        placeholder="Price"
-        type="date"
-        bind:value={startDate}
-      />
-    </div>
-  </div>
-  <div>
-    <div class="grid grid-cols-2 gap-x-4">
-      <Button
-        label="Cancel"
-        variant="variant-filled"
-        onClick={() => modalStore.close()}
-      />
-      <Button
-        label="Create"
-        variant="variant-filled-secondary"
-        onClick={handleCalenderAdd}
-      />
-    </div>
-  </div>
+	<div class="flex flex-col justify-center items-center">
+		<p class="text-2xl mt-4 font-semibold">Add a new listing to your calender</p>
+		<p class="px-4 mt-4">
+			Choose the date and price you want to start your content at, after that each item will be
+			posted every week, you can adjust your settings in the marketing page
+		</p>
+	</div>
+	<div class="my-8">
+		<div class="mb-4">
+			<Input label="Price" placeholder="Price" type="text" bind:value={price} />
+		</div>
+		<div>
+			<Input label="Date" placeholder="Price" type="date" bind:value={startDate} />
+		</div>
+	</div>
+	<div>
+		<div class="grid grid-cols-2 gap-x-4">
+			<Button label="Cancel" variant="variant-filled" onClick={() => modalStore.close()} />
+			<Button label="Create" variant="variant-filled-secondary" onClick={handleCalenderAdd} />
+		</div>
+	</div>
 </div>

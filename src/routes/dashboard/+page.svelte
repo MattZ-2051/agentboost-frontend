c0466e7c2f01e5956ff9fdb01c857f522e36e8f2
Matt Zamora <mzamora2899@gmail.com>
2023-11-20 19:05:39 -0700

    add tab comp

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
 </script>
 
-<div class="" />
+<div class="">
+	<div class="w-full">
+		<Tabs
+			items={[
+				{
+					label: 'tab1',
+					name: 'tab1'
+				},
+				{
+					label: 'tab2',
+					name: 'tab2'
+				}
+			]}
+		/>
+	</div>
+</div>

cd262b2182653086dc157e57ce1d60da53c2eec6
Matt Zamora <mzamora2899@gmail.com>
2023-12-06 12:23:27 -0700

    marketing page wip

--- src/lib/pages/Marketing/SuggestedCampaigns/CampaignBox.svelte
+++ src/lib/pages/Marketing/SuggestedCampaigns/CampaignBox.svelte
@@ -15,20 +15,26 @@
 	<div class="mt-4">
 		<p class="text-[#6F767E] font-medium text-sm">Target audience</p>
 		<div
-			class="w-[125px] h-[34px] px-2.5 py-[5px] bg-zinc-800 rounded-[10px] flex-col justify-center items-center gap-2.5 inline-flex"
+			class="w-[125px] h-[34px] px-2.5 py-[5px] bg-[#22272A] rounded-[10px] flex items-center justify-center mt-4 mb-6"
 		>
-			<div class="justify-start items-center gap-2.5 inline-flex">
-				<div class="text-neutral-300 text-[13px] font-medium font-['Inter'] leading-normal">
-					First time buyers
-				</div>
-			</div>
+			<p class="text-neutral-300 text-[13px] font-medium whitespace-nowrap leading-normal">
+				First time buyers
+			</p>
+		</div>
+		<div>
+			<p class="text-[#6F767E] text-[14px] font-medium">Description</p>
+			<p class="mt-4 text-sm text-[#CFD0D5] leading-5">
+				Lorem ipsum dolor sit amet consectetur. Amet habitasse at aliquam imperdiet sit sit tempus.
+				Enim purus lectus diam odio pharetra nisi et ullamcorper. Mauris egestas et at at arcu.
+				Purus condimentum ut ornare semper ut.
+			</p>
 		</div>
 	</div>
 </div>
 
 <style lang="postcss">
 	.campaignBox {
-		@apply bg-[#1E2225] rounded-[10px] border border-white border-opacity-20 w-full h-[330px] p-6;
+		@apply bg-[#1E2225] rounded-[10px] border border-white border-opacity-10 w-full h-[350px] p-6;
 	}
 
 	.myCampaignBox {
--- src/lib/pages/Marketing/SuggestedCampaigns/SuggestedCampaigns.svelte
+++ src/lib/pages/Marketing/SuggestedCampaigns/SuggestedCampaigns.svelte
@@ -3,7 +3,7 @@
 	import CampaignBox from './CampaignBox.svelte';
 </script>
 
-<div class="w-full h-[440px] px-6 py-8 bg-[#141718] rounded-[10px]">
+<div class="w-full h-full px-6 py-8 bg-[#141718] rounded-[10px]">
 	<div class="flex items-center gap-x-4 mb-6">
 		<CampaignsIcon />
 		<p class="text-white text-2xl font-semibold">Suggested Campaigns</p>

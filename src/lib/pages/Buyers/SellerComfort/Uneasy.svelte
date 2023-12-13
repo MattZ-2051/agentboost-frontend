5333242024878ec14ec7c8a932a07b126b840b9f
Matt Zamora <mzamora2899@gmail.com>
2023-12-12 17:46:53 -0700

    buyers id page wip

---
+++ src/lib/pages/Buyers/SellerComfort/Uneasy.svelte
@@ -0,0 +1,53 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="87" height="87" viewBox="0 0 87 87" fill="none">
+	<g filter="url(#filter0_d_1066_766)">
+		<path
+			d="M81 43.5C81 64.2107 64.2107 81 43.5 81C22.7893 81 6 64.2107 6 43.5C6 22.7893 22.7893 6 43.5 6C64.2107 6 81 22.7893 81 43.5ZM17.25 43.5C17.25 57.9975 29.0025 69.75 43.5 69.75C57.9975 69.75 69.75 57.9975 69.75 43.5C69.75 29.0025 57.9975 17.25 43.5 17.25C29.0025 17.25 17.25 29.0025 17.25 43.5Z"
+			fill="#EEEEEE"
+		/>
+	</g>
+	<path
+		d="M75.375 43.5C78.4816 43.5 81.0432 46.0323 80.5789 49.104C80.126 52.1005 79.3105 55.038 78.1455 57.8506C76.2609 62.4003 73.4987 66.5343 70.0165 70.0165C66.5343 73.4987 62.4003 76.2609 57.8506 78.1455C53.3009 80.03 48.4246 81 43.5 81C38.5754 81 33.6991 80.03 29.1494 78.1455C24.5997 76.2609 20.4657 73.4987 16.9835 70.0165C13.5013 66.5343 10.7391 62.4003 8.85452 57.8506C7.68948 55.038 6.87398 52.1005 6.42109 49.104C5.95684 46.0322 8.5184 43.5 11.625 43.5C14.7316 43.5 17.1898 46.0465 17.8504 49.0821C18.1818 50.6049 18.6489 52.0987 19.2482 53.5454C20.5673 56.7302 22.5009 59.624 24.9384 62.0616C27.376 64.4991 30.2698 66.4327 33.4546 67.7518C36.6394 69.071 40.0528 69.75 43.5 69.75C46.9472 69.75 50.3606 69.071 53.5454 67.7518C56.7302 66.4327 59.624 64.4991 62.0616 62.0616C64.4991 59.624 66.4327 56.7302 67.7518 53.5454C68.3511 52.0987 68.8182 50.6049 69.1496 49.0821C69.8102 46.0465 72.2684 43.5 75.375 43.5Z"
+		fill="url(#paint0_linear_1066_766)"
+	/>
+	<defs>
+		<filter
+			id="filter0_d_1066_766"
+			x="0.412698"
+			y="0.412698"
+			width="86.1746"
+			height="86.1746"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feMorphology
+				radius="5.5873"
+				operator="dilate"
+				in="SourceAlpha"
+				result="effect1_dropShadow_1066_766"
+			/>
+			<feOffset />
+			<feComposite in2="hardAlpha" operator="out" />
+			<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.07 0" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1066_766" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1066_766" result="shape" />
+		</filter>
+		<linearGradient
+			id="paint0_linear_1066_766"
+			x1="6"
+			y1="43.5"
+			x2="81"
+			y2="43.5"
+			gradientUnits="userSpaceOnUse"
+		>
+			<stop stop-color="#EFBC06" />
+			<stop offset="1" stop-color="#FBE155" />
+		</linearGradient>
+	</defs>
+</svg>

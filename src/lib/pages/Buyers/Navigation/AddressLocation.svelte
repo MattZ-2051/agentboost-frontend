7ace0ec2d1c2ee277ad11075d16dd91ace1f8bfa
Matt Zamora <mzamora2899@gmail.com>
2023-12-14 14:04:55 -0700

    buyers id page wip

--- src/lib/pages/Buyers/Navigation/AddressLocation.svelte
+++ src/lib/pages/Buyers/Navigation/AddressLocation.svelte
@@ -1,2 +1,23 @@
 <script lang="ts">
+	import LocationIconOutlined from '$lib/assets/svg/Location/LocationIconOutlined.svelte';
 </script>
+
+<div
+	class="bg-[#1E2225] w-full h-[86px] flex items-center gap-x-4 rounded-[10px] p-4 border-white border-opacity-20 border"
+>
+	<input class="checkbox w-[31px] h-[31px] rounded-[8px]" type="checkbox" />
+	<div class="bg-[#1D3335] rounded-full p-3">
+		<LocationIconOutlined />
+	</div>
+	<div>
+		<p class="text-white text-[15px] font-semibold">10044 W Springdale Ct</p>
+		<p class="text-xs text-[#A0A1AB] font-normal mt-1">2.5 miles from city center</p>
+	</div>
+</div>
+
+<style lang="postcss">
+	input:not(:checked) {
+		background-color: #1e2225;
+		@apply border-white border-opacity-20;
+	}
+</style>
---
+++ src/lib/pages/Buyers/Navigation/RealtorInfo.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import BuyersIcon from '$lib/assets/svg/BuyersIcon.svelte';
+	import FacebookDarkIcon from '$lib/assets/svg/Social/FacebookDarkIcon.svelte';
+	import TwitterDarkIcon from '$lib/assets/svg/Social/TwitterDarkIcon.svelte';
+	import InstagramDarkIcon from '$lib/assets/svg/Social/InstagramDarkIcon.svelte';
+</script>
+
+<div class="w-[382px]">
+	<div
+		class="bg-[#1E2225] w-full h-12 flex gap-x-2 items-center px-4 py-2 rounded-[10px] border border-white border-opacity-20"
+	>
+		<BuyersIcon iconSize={20} />
+		<p class="text-[#C6C7CD] text-base">10044 W Springdale Ct</p>
+	</div>
+	<div
+		class="bg-[#1E2225] w-full h-full flex-col flex gap-y-4 p-6 rounded-[10px] border border-white border-opacity-20 mt-6"
+	>
+		<div class="flex items-center w-full justify-between">
+			<div class="flex w-full items-center gap-x-2">
+				<img
+					class="w-[60px] h-[60px] rounded-[50px]"
+					src="https://via.placeholder.com/60x60"
+					alt=""
+				/>
+				<p class="text-[#EFEFEF] text-base">John Doe</p>
+			</div>
+			<div class="flex items-center gap-x-2">
+				<FacebookDarkIcon />
+				<InstagramDarkIcon />
+			</div>
+		</div>
+		<div class="flex w-full items-center">
+			<div class="w-1/4">
+				<p class="realtorInfoLabel">Agent ID:</p>
+				<p class="realtorInfoLabel">Phone:</p>
+				<p class="realtorInfoLabel">Email:</p>
+			</div>
+			<div class="w-3/4">
+				<p class="realtorInfoText">#18457 865 8745</p>
+				<p class="realtorInfoText">+1 123 123 4567</p>
+				<p class="realtorInfoText">johndoe@smartproperties.com</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.realtorInfoLabel {
+		@apply text-[#6F767E] text-sm;
+	}
+
+	.realtorInfoText {
+		@apply text-[#EFEFEF] text-sm;
+	}
+</style>

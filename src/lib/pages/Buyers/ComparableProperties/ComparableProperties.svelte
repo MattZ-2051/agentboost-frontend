4bb9a1205729879b27aaacf1f966415c2afd7430
Matt Zamora <mzamora2899@gmail.com>
2023-12-14 18:53:03 -0700

    listings page id wip

---
+++ src/lib/pages/Buyers/ComparableProperties/ComparableProperties.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ComparablePropertiesIcon from '$lib/assets/svg/ComparablePropertiesIcon.svelte';
+	import Slider from '$lib/components/Slider/Slider.svelte';
+	import ComparableCard from './ComparableCard.svelte';
+</script>
+
+<div class="bg-[#151719] rounded-[20px] w-full h-full p-8 flex flex-col">
+	<div class="flex items-center gap-x-2">
+		<ComparablePropertiesIcon />
+		<p class="text-[#FFF] font-semibold text-[24px]">Comparable Properties</p>
+	</div>
+	<div>
+		<Slider length={10} classes="gap-x-8 w-[90vw]">
+			<ComparableCard />
+		</Slider>
+	</div>
+</div>
---
+++ src/lib/pages/Buyers/Insights/Insights.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import InfoIconHover from '$lib/assets/svg/InfoIconHover.svelte';
+</script>
+
+<div class="bg-[#151719] rounded-[20px] w-full h-full p-8 flex flex-col">
+	<div class="flex items-center gap-x-2 text-[#fff] font-semibold text-[18px]">
+		<InfoIconHover />
+		<p>Insights</p>
+	</div>
+	<div class="grid grid-cols-3 gap-x-8">
+		<div class="infoBox">
+			<p class="text-[#CFD0D5] text-sm">
+				Based on the average days on market for homes in WESTVIEW SUB NO 01, which is 75.3 days, the
+				fact that the most recent home sold in just 24 days indicates that the subject home has a
+				potential advantage in terms of its desirability and appeal.
+			</p>
+		</div>
+		<div class="infoBox">
+			<p class="text-[#CFD0D5] text-sm">
+				Based on the average days on market for homes in WESTVIEW SUB NO 01, which is 75.3 days, the
+				fact that the most recent home sold in just 24 days indicates that the subject home has a
+				potential advantage in terms of its desirability and appeal.
+			</p>
+		</div>
+		<div class="infoBox">
+			<p class="text-[#CFD0D5] text-sm">
+				Based on the average days on market for homes in WESTVIEW SUB NO 01, which is 75.3 days, the
+				fact that the most recent home sold in just 24 days indicates that the subject home has a
+				potential advantage in terms of its desirability and appeal.
+			</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.infoBox {
+		@apply bg-[#1E2225] w-full h-[140px] rounded-[10px] py-4 px-5 border border-white border-opacity-20;
+	}
+</style>

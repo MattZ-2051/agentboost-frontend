2a46340a5c6d36b3a2ccd89d4f64aaf5451bbc05
Matt Zamora <mzamora2899@gmail.com>
2023-12-26 17:28:39 -0700

    profile page wip

---
+++ src/routes/profile/[username]/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import UserInfo from '$lib/pages/Profile/UserInfo/UserInfo.svelte';
+	import type { TabItems } from '$types/components';
+
+	const tabItems: TabItems = [
+		{
+			label: 'Profile'
+		},
+		{
+			label: 'Subscription'
+		},
+		{
+			label: 'Give feedback'
+		},
+		{ label: 'Contact support' },
+		{
+			label: 'Refer & earn'
+		}
+	];
+
+	$: selectedItem = 'Profile';
+</script>
+
+<div>
+	<div class="h-[1px] bg-[#181818] w-full my-6" />
+	<h1 class="text-[24px] font-semibold mb-8">Account</h1>
+	<div class="bg-[#171A1C] rounded-[20px] w-full h-full p-6">
+		<Tabs items={tabItems} {selectedItem}>
+			<div slot="content" class="mt-6">
+				{#if selectedItem === 'Profile'}
+					<div>
+						<UserInfo />
+					</div>
+				{/if}
+			</div>
+		</Tabs>
+	</div>
+</div>

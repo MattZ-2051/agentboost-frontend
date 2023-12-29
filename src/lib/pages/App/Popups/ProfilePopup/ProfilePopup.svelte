ca0691b0b1f1658afdb9916b1446f9c521c08cb8
Matt Zamora <mzamora2899@gmail.com>
2023-12-28 17:04:20 -0700

    wip

--- src/lib/pages/App/Popups/ProfilePopup/ProfilePopup.svelte
+++ src/lib/pages/App/Popups/ProfilePopup/ProfilePopup.svelte
@@ -3,30 +3,39 @@
 	import Icon from '@iconify/svelte';
 	import Button from '$lib/components/Button/Button.svelte';
 	import LogoutIcon from '$lib/assets/svg/LogoutIcon.svelte';
+	import { goto } from '$app/navigation';
 
 	$: iconHover = false;
+
+	const username = 'jacobjones';
 </script>
 
 <div
 	transition:fade={{ delay: 0, duration: 100 }}
-	class="w-[247px] h-[152px] bg-zinc-900 absolute top-10 -right-4 z-50 rounded-[10px] shadow p-4 flex flex-col gap-y-4"
+	class="w-[247px] h-[182px] bg-[#1A1A1A] absolute top-10 -right-4 z-50 rounded-[10px] shadow p-4 flex flex-col gap-y-4"
 >
-	<div class="flex items-start">
-		<div class="mr-2 text-base mt-1">
-			<Icon icon="solar:settings-linear" />
-		</div>
-		<div class="">
-			<p class="text-white text-[15px]">My Account</p>
-			<p class="text-[13px] text-zinc-400">Edit Account Info</p>
+	<div
+		class="flex items-start hover:bg-white hover:bg-opacity-[0.05] py-1 px-2 rounded-[10px]"
+		on:click={() => goto(`/profile/${username}`)}
+	>
+		<div class="px-3 flex hover:cursor-pointer">
+			<div class="mr-2 text-base mt-1">
+				<Icon icon="solar:settings-linear" />
+			</div>
+			<div>
+				<p class="text-white text-[15px]">My Account</p>
+				<p class="text-[13px] text-zinc-400">Edit Account Info</p>
+			</div>
 		</div>
 	</div>
-	<div class="w-full h-[1px] border border-zinc-800" />
+	<div class="w-full h-[1px] border border-[#292929]" />
 	<Button
 		label="Logout"
 		variant="variant-app-primary"
 		onMouseEnter={() => (iconHover = true)}
 		onMouseLeave={() => (iconHover = false)}
-		bg={'bg-zinc-900'}
+		bg={'bg-[#1A1A1A]'}
+		classes="!h-[42px] !w-full"
 	>
 		<span slot="icon" class="mr-2 mt-[0.5px]">
 			{#if !iconHover}
--- src/lib/pages/Profile/UserInfo/UserInfo.svelte
+++ src/lib/pages/Profile/UserInfo/UserInfo.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import CameraIcon from '$lib/assets/svg/CameraIcon.svelte';
+	import DangerIconBlue from '$lib/assets/svg/DangerIconBlue.svelte';
+	import Input from '$lib/components/Input/Input.svelte';
 </script>
 
 <div>
@@ -6,4 +9,58 @@
 	<p class="text-[#707281] text-[15px] mt-2">
 		This page displays general information about your account.
 	</p>
+</div>
+<div class="grid grid-cols-2 gap-x-[26px] w-[90%]">
+	<div class="mt-6 flex gap-x-2">
+		<img class="w-20 h-20 relative rounded-[60px]" src="https://via.placeholder.com/80x80" alt="" />
+		<div>
+			<p class="text-[#707281] text-[15px] mb-[6px]">Profile image</p>
+			<div
+				class="bg-[#2E2F37] rounded-[90px] py-[6px] px-[16px] flex items-center justify-center h-[36px] w-[192px] gap-x-[10px]"
+			>
+				<div>
+					<CameraIcon />
+				</div>
+				<p class="text-[#C6C7CD] text-[15px] whitespace-nowrap">Upload new image</p>
+			</div>
+		</div>
+	</div>
+	<div class="mt-6 flex gap-x-2">
+		<img class="w-20 h-20 relative rounded-[60px]" src="https://via.placeholder.com/80x80" alt="" />
+		<div>
+			<p class="text-[#707281] text-[15px] mb-[6px]">Profile image</p>
+			<div
+				class="bg-[#2E2F37] rounded-[90px] py-[6px] px-[16px] flex items-center justify-center h-[36px] w-[192px] gap-x-[10px]"
+			>
+				<div>
+					<CameraIcon />
+				</div>
+				<p class="text-[#C6C7CD] text-[15px] whitespace-nowrap">Upload new image</p>
+			</div>
+		</div>
+	</div>
+	<div class="mt-8">
+		<Input
+			label="Full Name"
+			value="Jacob Jones"
+			variant="variant-app-primary"
+			classes="!mt-[6px]"
+			type="text"
+			placeholder="Full Name"
+		/>
+	</div>
+	<div class="mt-8">
+		<Input
+			label="Email"
+			value="jacob@smartproperties.com"
+			variant="variant-app-primary"
+			classes="!mt-[6px]"
+			type="text"
+			placeholder="Connect Email"
+		>
+			<span slot="icon">
+				<DangerIconBlue />
+			</span>
+		</Input>
+	</div>
 </div>
